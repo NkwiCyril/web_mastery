3aab24611e95dc2e81cb92d12105f8670baeeb6e
NkwiCyril <akinimbomnkwi@gmail.com>
2023-12-28 19:49:09 +0100

    using the different selectorsin javascript

--- 0.0 javascript/DOM/DOM Challenge Starting Files/index.js
+++ 0.0 javascript/DOM/DOM Challenge Starting Files/index.js
@@ -1,3 +1,5 @@
 document.querySelector("ul").lastElementChild.innerHTML = "Nkwi Cyril";
-
-document.querySelector("li a")``.style.color = "orange";
+document.querySelector("li a").style.color = "orange";
+document.querySelector("button").setAttribute('class', 'change') // set new attribute of button tag
+document.querySelector('h1').textContent = "Hello Cyril"
+document.querySelector('h1').innerHTML = "<em>Hello Cyril</em>"
