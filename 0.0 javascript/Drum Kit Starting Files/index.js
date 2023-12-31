6a5ccd0ad340fd0887fa9933ea065f92d64beac2
NkwiCyril <akinimbomnkwi@gmail.com>
2023-12-31 07:42:02 +0100

    added the button animation function to animate after 0.1s on click and keypress

--- 0.0 javascript/Drum Kit Starting Files/index.js
+++ 0.0 javascript/Drum Kit Starting Files/index.js
@@ -5,51 +5,21 @@ var handleClick = () => {
 var btns = document.querySelectorAll(".drum"); // get length of elements with class of drum
 console.log(btns[0]);
 // using the for-each loop to add event listeners to all elements with class .drum
-// btns.forEach((btn) => {
-//   btn.addEventListener("click", function () {
-//     console.log(btn.innerHTML);
-//     switch (btn.innerHTML) {
-//       case "w":
-//         var tom1 = new Audio("./sounds/tom-1.mp3");
-//         tom1.play();
-//         break;
-//       case "a":
-//         var tom2 = new Audio("./sounds/tom-2.mp3");
-//         tom2.play();
-//         break;
-//       case "s":
-//         var tom3 = new Audio("./sounds/tom-3.mp3");
-//         tom3.play();
+btns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    makeSound(btn.innerHTML);
+    buttonAnimation(btn.innerHTML);
+    console.log(btn);
+  });
+});
 
-//         break;
-//       case "d":
-//         var tom4 = new Audio("./sounds/tom-4.mp3");
-//         tom4.play();
+document.addEventListener("keydown", function (e) {
+  makeSound(e.key);
+  buttonAnimation(e.key);
+});
 
-//         break;
-//       case "j":
-//         var crash = new Audio("./sounds/crash.mp3");
-//         crash.play();
-//         break;
-//       case "k":
-//         var kBass = new Audio("./sounds/kick-bass.mp3");
-//         kBass.play();
-
-//         break;
-//       case "l":
-//         var snare = new Audio("./sounds/snare.mp3");
-//         snare.play();
-//         break;
-
-//       default:
-//         alert("Sounds not available at the moment.");
-//         break;
-//     }
-//   });
-// });
-
-document.addEventListener("keypress", (e) => {
-  switch (e.key) {
+function makeSound(e) {
+  switch (e) {
     case "w":
       var tom1 = new Audio("./sounds/tom-1.mp3");
       tom1.play();
@@ -86,4 +56,12 @@ document.addEventListener("keypress", (e) => {
       alert("Sounds not available at the moment.");
       break;
   }
-});
+}
+
+function buttonAnimation(currentButton) {
+  var activeButton = document.querySelector("." + currentButton);
+  activeButton.classList.add("pressed");
+  setTimeout(() => {
+    activeButton.classList.remove("pressed")
+  }, 100);
+}d
