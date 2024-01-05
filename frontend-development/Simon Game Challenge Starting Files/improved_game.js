9cc347c0dd0791f046a37b68627a906f21f7ca2e
NkwiCyril <akinimbomnkwi@gmail.com>
2024-01-05 21:38:34 +0100

    reinitializing the nextSequence function.

--- frontend-development/Simon Game Challenge Starting Files/improved_game.js
+++ frontend-development/Simon Game Challenge Starting Files/improved_game.js
@@ -3,9 +3,10 @@ var userPattern = []; // store pattern selected by user
 
 var buttonColors = ["red", "blue", "green", "yellow"]; // colors of the various buttons
 
-var nextSequence = () => {
-  return Math.floor(Math.random() * (buttonColors.length + 1));
-};
+function nextSequence() {
+  let randomNumber = Math.floor(Math.random() * (buttonColors.length + 1));
+  let randomChosenColor = buttonColors[randomNumber]; // get color randomly
+  gamePattern.push(randomChosenColor); // store random selected color in gamePattern list
 
 $("body").keypress(function (e) {
   while (true) {
