574b2f72c20180b37583a3495a61c4a310fa9c43
NkwiCyril <akinimbomnkwi@gmail.com>
2024-01-08 05:07:32 +0100

    worked out the game properly; now evaluates each choice taken by the user and game

--- frontend-development/Simon Game Challenge Starting Files/improved_game.js
+++ frontend-development/Simon Game Challenge Starting Files/improved_game.js
@@ -1,6 +1,7 @@
 var gamePattern = []; // store random colors selected by comp
 var userClickedPattern = []; // store pattern selected by user
-
+var level = 0;
+var started = false;
 var buttonColors = ["red", "blue", "green", "yellow"]; // colors of the various buttons
 
 function playSound(colorName) {
@@ -12,22 +13,22 @@ function nextSequence() {
   var randomNumber = Math.floor(Math.random() * buttonColors.length);
   var randomChosenColor = buttonColors[randomNumber]; // get color randomly
   gamePattern.push(randomChosenColor); // store random selected color in gamePattern list
-  console.log("updated game pattern: " + gamePattern);
+  console.log("game pattern: " + gamePattern);
 
-  var level = 0;
   while (level < gamePattern.length) {
+    // increase level number when nextSequence is called
     $("#level-title").text(`Level ${level + 1}`);
     level++;
   }
 
   playSound(randomChosenColor);
   setTimeout(() => {
-    $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100);
+    $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100); // blink button selected by computer
   }, 100);
 }
 
 function animatePress(currentColor) {
-  // get currentColor of clicked button and add .pressed
+  // get currentColor of clicked button and add .pressed (check css file for style)
   $("." + currentColor).addClass("pressed");
   setTimeout(() => {
     $("." + currentColor).removeClass("pressed");
@@ -35,36 +36,52 @@ function animatePress(currentColor) {
 }
 
 $("body").keypress(function () {
-  var title = $("#level-title").text();
-  if (title == "Press A Key to Start") {
-    nextSequence();
-  }
+  // if (
+  //   title == "Press A Key to Start" ||
+  //   title == "Game Over, Press Any Key to Restart"
+  // ) {
+  //   nextSequence();
+  // }
+
+	if (!started) {
+		nextSequence();
+	} 
+
+});
+
 $(".btn").click(function () {
   var userChosenColor = $(this).attr("id"); // select the current HTML element
   animatePress(userChosenColor);
   userClickedPattern.push(userChosenColor);
-  console.log("updated user pattern: " + userClickedPattern);
-    playSound(userChosenColor);
+  console.log("user pattern: " + userClickedPattern);
+  playSound(userChosenColor);
   checkAnswer(userClickedPattern.lastIndexOf(userChosenColor));
 });
 
 function checkAnswer(checkLevel) {
   console.log(checkLevel);
-
   if (userClickedPattern[checkLevel] === gamePattern[checkLevel]) {
-    console.log("success");
-		if (userClickedPattern.length === gamePattern.length) {
-			setTimeout(() => {
-				userClickedPattern = []
-				nextSequence()
-		} {
-			alert('Incorrect sequence!')
-		}
-  });
-});
+    // console.log("success");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(() => {
+        userClickedPattern = [];
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    // console.log("wrong");
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(() => {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("h1").text("Game Over, Press Any Key to Restart");
+		startOver();
+  }
+}
 
-function checkAnswer(a, b) {
-  let lengthCompare = a.length === b.length;
-  let elementCompare = a.every((element, index) => element === b[index]);
-  return lengthCompare && elementCompare;
+function startOver() {
+	userClickedPattern = []
+	gamePattern = []
+	level = 0;
 }
