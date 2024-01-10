c5b58dfced32b5bd5211d5b4a0d648d28857dc5a
NkwiCyril <akinimbomnkwi@gmail.com>
2024-01-10 05:21:11 +0100

    using node REPL

---
+++ backend/2.1 Using Node/index.js
@@ -0,0 +1,9 @@
+// be the best in what you do. take a nap, wake up and be good.
+
+console.log("Hello Node");
+
+let arr = ["Cyril", "Ajimsimbom", "Noura", "Afu"]
+
+let newArr = arr.map((e) => e + " Nkwi")
+
+console.log(newArr); 
