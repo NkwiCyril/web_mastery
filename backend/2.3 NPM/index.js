7f38c9b9f9087e4da0e98cc3eb080ca96e3e2b40
NkwiCyril <akinimbomnkwi@gmail.com>
2024-01-10 06:36:49 +0100

    working with the node package manager

---
+++ backend/2.3 NPM/index.js
@@ -0,0 +1,9 @@
+import generateName from "sillyname";
+import superHero from "superheroes";
+
+var newName = generateName();
+
+// console.log("My real name is " + newName);
+// console.log("My real name is " + superHero.random());
+
+console.log(superHero.all);
