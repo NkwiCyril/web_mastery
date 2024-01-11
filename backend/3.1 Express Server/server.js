7fc97fa6d948b79a9d0b6a96e21fb56173f16e72
NkwiCyril <akinimbomnkwi@gmail.com>
2024-01-11 08:36:44 +0100

    successfully worked with the get() express method

---
+++ backend/3.1 Express Server/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`server running on port http://localhost:${port}`);
+});
+
+app.get("/data",(req, res) => {
+
+    res.send("<h1>Welcome! Your server is successfully running.</h1>")
+
+})
