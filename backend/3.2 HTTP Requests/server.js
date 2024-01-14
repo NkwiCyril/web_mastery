a13d2be1b9ba61ff876eeae3b198b7011cbd6ec5
NkwiCyril <akinimbomnkwi@gmail.com>
2024-01-14 07:37:01 +0100

    adding comments

--- backend/3.2 HTTP Requests/server.js
+++ backend/3.2 HTTP Requests/server.js
@@ -2,18 +2,20 @@ import express from "express";
 
 const app = express();
 
-app.listen(5000, () => {
-  console.log("successfully running!");
-});
-
 app.get("/", (req, res) => {
   res.send("<h1>Welcome Sir.This is the server running</h1>");
 });
 
-app.get("/about", (req, res) => {
+// setting up all endpoints
+app.get("/about", (req, res) => { 
     res.send("<h1>Hello, I am Nkwi Cyril. A full-stack developer</h1>")
 })
 
 app.get("/contact", (req, res) => {
     res.send("<h1>Contact Me <br>+237 651085550</h1>")
 })
+
+// listening for the available port available
+app.listen(5000, () => {
+  console.log("successfully running!");
+});
