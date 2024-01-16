d4ab258da88e20dfac339e9b58ca98cd41a5f612
NkwiCyril <akinimbomnkwi@gmail.com>
2024-01-16 04:24:29 +0100

    added comment on all type of requests used in index file

--- backend/3.3 Postman/index.js
+++ backend/3.3 Postman/index.js
@@ -2,7 +2,6 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-
 // *********************
 // Let’s practice using Postman. Make sure your server is running with nodemon.
 // Then test the 5 different routes below with Postman. Open a separate tab for each request.
@@ -10,23 +9,31 @@ const port = 3000;
 // You should not get any 404s or 500 status codes.
 // *********************
 
+// only used to retrieve data from the server in which it is hosted (no modification is done on the data)
 app.get("/", (req, res) => {
   res.send("<h1>Home Page</h1>");
 });
 
+// used to send data to the server for processing. 
+// usually changes the state of the server and other side effects
 app.post("/register", (req, res) => {
   //Do something with the data
   res.sendStatus(201);
 });
 
+// updates a current resource or create it if it does not exist, 
+// the client is the one that provides the complete and updated copy of the resource
 app.put("/user/cyril", (req, res) => {
   res.sendStatus(200);
 });
 
+// updates parts of an existing resource
+// client provides the parts of the resource that need updating
 app.patch("/user/cyril", (req, res) => {
   res.sendStatus(299);
 });
 
+// removes specified resource from the server
 app.delete("/user/cyril", (req, res) => {
   //Deleting
   res.sendStatus(200);
