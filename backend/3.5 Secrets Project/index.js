4a0a90f3ceb8f50e02f7b4e2f716acbd306174d9
NkwiCyril <akinimbomnkwi@gmail.com>
2024-01-18 14:26:36 +0100

    added my own custom middleware; passwordParser

--- backend/3.5 Secrets Project/index.js
+++ backend/3.5 Secrets Project/index.js
@@ -7,24 +7,33 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
 
-var __fileName = fileURLToPath(import.meta.url);
-var __dirname = dirname(__fileName);
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(__fileName);
 
 const app = express();
 const port = 5000;
+var authorized = false;
 
 app.use(bodyParser.urlencoded({ extended: true })); // make use of the imported middleware
+
+var passwordParser = (req, res, next) => {
+  const password = req.body["password"];
+  if (password == "ILoveProgramming.") authorized = true; // check if password is the correct password in order to access secret file
+  next();
+};
+
+app.use(passwordParser);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html"); // display file from its location
 });
 
 app.post("/check", (req, res) => {
-  req.body.password === "ILoveProgramming." // check if password is the correct password in order to access secret file
-    ? setInterval(() => {
-        res.sendFile(__dirname + "/public/secret.html");
-      }, 2000) // delay for some time before displaying the secret file
-    : res.sendFile(__dirname + "/public/error.html");
+  if (authorized)
+    // check if password is the correct password in order to access secret file
+    setInterval(() => {
+      res.sendFile(__dirname + "/public/secret.html");
+    }, 2000); // delay for some time before displaying the secret file
 });
 
 app.listen(port, () => {
