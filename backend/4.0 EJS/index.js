b6b21b7fc8f517d4bf4ca85e7a9b465e920b01c3
NkwiCyril <akinimbomnkwi@gmail.com>
2024-01-21 17:12:13 +0100

    working on server side script

--- backend/4.0 EJS/index.js
+++ backend/4.0 EJS/index.js
@@ -1,9 +1,17 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+const PORT = 5000;
 
-const PORT = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-app.listen("/", (req, res) => {
-  console.log("steady on port 3000");
+app.listen(PORT, () => {
+  console.log(`Steady on http://localhost/${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.ejs");
 });
