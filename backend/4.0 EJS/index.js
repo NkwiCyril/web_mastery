3d919bdaa3078a1ff5b37cf48cd4cfe4d2395890
NkwiCyril <akinimbomnkwi@gmail.com>
2024-01-24 19:39:33 +0100

    changed port number; avoid port clash

--- backend/4.0 EJS/index.js
+++ backend/4.0 EJS/index.js
@@ -3,7 +3,7 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
