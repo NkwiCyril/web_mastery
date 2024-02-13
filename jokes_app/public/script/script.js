68fe664331f70976c23cab2260a1021650f25856
NkwiCyril <akinimbomnkwi@gmail.com>
2024-02-13 20:33:49 +0100

    REST:API

---
+++ jokes_app/public/script/script.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector("button");
+const setCat = document.querySelector(".set-category");
+
+btn.addEventListener("click", () => {
+    location.reload();
+})
---
+++ jokes_app/server.js
@@ -0,0 +1,55 @@
+import express from "express";
+import axios from "axios";
+import "dotenv/config";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const API_URL = process.env.API_URL;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/joke", async (req, res) => {
+  const category = req.body["category"];
+  const type = req.body["type"];
+
+  if (type === "single") {
+    try {
+      const response = await axios.get(API_URL + category + "?type=" + type);
+      const data = response.data;
+      res.render("joke.ejs", {
+        result: data,
+      });
+    } catch (error) {
+      error.message =
+        "Error encountered in getting joke. Please try again later!";
+      res.render("joke.ejs", {
+        error: error.message,
+      });
+    }
+  } else if (type === "twopart"){
+		try {
+      const response = await axios.get(API_URL + category + "?type=" + type);
+      const data = response.data;
+      res.render("joke.ejs", {
+				type: type,
+        result: data,
+      });
+    } catch (error) {
+      error.message =
+        "Error encountered in getting joke. Please try again later!";
+      res.render("joke.ejs", {
+        error: error.message,
+      });
+    }
+	}
+});
+
+app.listen(PORT, () => {
+  console.log(`Live on http://localhost:${PORT}`);
+});
