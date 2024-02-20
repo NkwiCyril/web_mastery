28daf025dc0adcfce0371ae4e294582c06c2defd
NkwiCyril <akinimbomnkwi@gmail.com>
2024-02-20 17:11:20 +0100

    required endpoints for note_api

--- note_pad/note_api.js
+++ note_pad/note_api.js
@@ -12,8 +12,38 @@ app.get("/", (req, res) => {
   res.status(200).send("Ready to go!");
 });
 
+// get all notes
+
+app.get("/notes", (req, res) => {
+
+});
+
+// get specific note
+
+app.get("/notes/:id", (req, res) => {
+
+})
+
+// create or post a note
+
+app.post("/notes", (req, res) => {
+
+});
+
+// modify a note partially
+
+app.patch("/notes/:id", (req, res) => {
+
+});
+
+// delete a note
+
+app.delete("/notes/:id", (req, res) => {
+
+})
+
 app.listen(PORT, () => {
-  console.log(`Steady on http:localhost:${PORT}`);
+  console.log(`Steady on http://localhost:${PORT}`);
 });
 
 // send("Live on port " + PORT)
