fbf6928e24b1c18f8f94708fd52cdb3df32b411d
NkwiCyril <akinimbomnkwi@gmail.com>
2024-02-24 20:57:35 +0100

    removed all predefined notes array

--- note_pad/model/notes.js
+++ note_pad/model/notes.js
@@ -1,18 +1,5 @@
 var notes = [
-  {
-    id: 1,
-    category: "Study",
-    content: "There are about 2.5 million people in Namibia.",
-    time: "12PM",
-    date: "20th Feb 2024",
-  },
-  {
-    id: 2,
-    category: "Work",
-    content: "Remember to submit the report by EOD.",
-    time: "9AM",
-    date: "21st Feb 2024",
-  },
+
 ];
 
 
