5f7e9329b280091da0333ccc63752fd6dc3c9005
NkwiCyril <akinimbomnkwi@gmail.com>
2024-02-27 23:30:03 +0100

    api to manage task resources

--- task_manager/api/task_manager_api.js
+++ task_manager/api/task_manager_api.js
@@ -5,6 +5,9 @@ import tasks from "../modules/tasks.js";
 const app = express();
 const PORT = 7000;
 
+var len = tasks.length;
+var lastId = tasks.lastIndexOf(tasks[len - 1]) + 1;
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -13,21 +16,74 @@ app.get("/", (req, res) => {
 });
 
 // get all tasks
-app.get("/tasks", (req, res) => {
+app.get("/api/tasks", (req, res) => {
   res.status(200).json(tasks);
 });
 
 // get task by id
-app.get("/tasks/:id", (req, res) => {
-    const id = req.params.id;
-  
-  });
+app.get("/api/tasks/:id", (req, res) => {
+  const id = req.params.id;
+  const foundById = tasks.find((task) => task.id === parseInt(id));
 
-// get task by status [in_progress, todo, done]
-app.get("/tasks/:status", (req, res) => {
-  const status = req.params.status;
-  
+  res.json(foundById);
 });
+
+// filter tasks by status [in_progress, todo, done]
+app.get("/api/filter", (req, res) => {
+  const { status } = req.query;
+  const filteredTasks = tasks.filter(
+    (task) => task.status.toLowerCase() === status.toLowerCase()
+  );
+
+  res.status(200).json(filteredTasks);
+});
+
+// create a task
+app.post("/api/tasks", (req, res) => {
+  const createdTask = {
+    id: (lastId += 1),
+    content: req.body.content,
+    status: req.body.status,
+    time: new Date().toLocaleTimeString(),
+    date: new Date().toLocaleDateString(),
+  };
+  tasks.push(createdTask);
+  if (tasks.includes(createdTask)) {
+    res.status(200).json({ message: "Task added successfully!" });
+  } else {
+    res.status(404).json({ message: "Task could not be added. Try again!" });
+  }
+});
+
+// edit a task
+app.patch("/api/tasks/:id", (req, res) => {
+  const id = req.params.id;
+  const searchTask = tasks.find((task) => task.id === parseInt(id));
+  if (!searchTask)
+    return res.status(404).json({ message: "Unable to find task." });
+
+  if (req.body.content) searchTask.content = req.body.content;
+  if (req.body.status) searchTask.status = req.body.status;
+
+  res.status(200).json(searchTask);
+});
+
+app.delete("/api/tasks/:id", (req, res) => {
+  const id = req.params.id;
+  const searchIndex = tasks.findIndex((task) => task.id === parseInt(id));
+  if (tasks.includes(tasks[searchIndex])) {
+    tasks.splice(searchIndex, 1);
+    res.status(200).json({
+      message: "task successfully deleted.",
+      response: tasks,
+    });
+  } else {
+    res.status(404).json({
+      message: "Unable to find task.",
+    });
+  }
+})
+
 
 
 app.listen(PORT, () => {
