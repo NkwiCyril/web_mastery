b5ae1ea64bb3019929144b097b6adf8498bfd8e8
NkwiCyril <akinimbomnkwi@gmail.com>
2024-02-28 16:38:34 +0100

    server running

--- task_manager/server.js
+++ task_manager/server.js
@@ -1,2 +1,26 @@
 import express from "express";
 import bodyParser from "body-parser";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.APP_PORT;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("start.ejs");
+});
+
+// get username from start.ejs and display here
+app.post("/", (req, res) => {
+  const username = req.body.username;
+//   setTimeout(() => {}, 2000);
+  res.render("index.ejs", {
+    name: username,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Application on http://localhost:${PORT}`);
+});
